ca93e78498580ba7622b29af61380614c92ac39b
Youssef Sabry <youssef.sabry@hotmail.com>
2023-03-16 14:27:12 -0400

    Refactor OneBus to use bus compliance util

--- heuristics/OneBus.py
+++ heuristics/OneBus.py
@@ -1,20 +1,18 @@
 from control.DistanceControl import DistanceControl
 from control.FleetControl import FleetControl
-from util.compliance import do_stops_satisfy_requests
+from util.compliance import do_stops_satisfy_requests, does_bus_satisfy_requests
 
 # This heuristic assigns all requests to the first bus
 def heuristic_one_bus(fleet_control: FleetControl, dc: DistanceControl, current_time: int):
     bus = fleet_control.busses[0]
 
     for req in fleet_control.request_pool:
-        potential_path = bus.path.get_copy()
-        potential_path.append_stop_if_not_in_path(req.start_location)
-        potential_path.append_stop_after_pred(req.destination, req.start_location)
-        potential_reqs = bus.passenger_requests.copy()
-        potential_reqs.add(req)
+        old_path = bus.path.get_copy()
 
-        if do_stops_satisfy_requests(current_time, bus.current_location, bus.current_passenger_count, potential_path, potential_reqs, dc):
-            bus.append_if_not_already_in_upcoming(req.start_location, req)
-            bus.append_after_stop(req.destination, req, req.start_location)
+        bus.append_request_to_path(req)
+
+        if not does_bus_satisfy_requests(current_time,bus, dc):
+            bus.path = old_path
+            bus.passenger_requests.remove(req)
 
     fleet_control.request_pool.clear()
